903f983e91510812fc2e224c2825fcba04673e04
2ndsprout <2ndsprout@gmail.com>
2024-03-13 09:07:14 +0900

    java board commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,11 @@
+package org.example;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        BoardApp app = new BoardApp();
+        app.run();
+    }
+}
